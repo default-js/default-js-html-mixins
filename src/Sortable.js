30640daa101aaccbdd26992e456037581090c286
Frank Schüler <SchuelerFrank@gmx.de>
2023-07-16 20:22:41 +0200

    1.0.1 more flexibility by sortable

--- src/Sortable.js
+++ src/Sortable.js
@@ -6,16 +6,37 @@ const ATTR__SORTABLE_ITEM_CONTAINER_SELECTOR = "sortable-item-container-selector
 const ATTR__SORTABLE_BY = "sortable-by";
 const ATTR__SORTABLE_DIRECTON = "sortable-direction";
 
+const getItemByAttribute = (item, attributeSelector) => {
+	if(!item) 
+		return null;
+
+	if(item.is(attributeSelector))
+		return item;
+	
+	return item.find(attributeSelector).first() || null;
+}
+
+const getValue = (item, attribute, attributeSelector) => {
+	item = getItemByAttribute(item, attributeSelector);
+	if(item != null)
+		return item.attr(attribute);
+
+	return null;
+} 
+
 BODY.on(EVENTTYPE, (event) => {
 	event.stopPropagation();
 	const target = event.target;
 	const sortContainer = target.find(target.attr(ATTR__SORTABLE_ITEM_CONTAINER_SELECTOR)).first();	
 	const valueAttribute = `${ATTR__SORTABLE_BY}--${target.attr(ATTR__SORTABLE_BY)}`;
+	const valueSelector = `[${valueAttribute}]`;
 	const direction = (target.attr(ATTR__SORTABLE_DIRECTON) || "").trim().toLowerCase();
-	
+	if(!sortContainer)
+		return;
+		
 	const sorted = Array.from(sortContainer.children).sort((a, b) => {				
-		const valueA = (a.attr(valueAttribute) || "").toLowerCase();
-		const valueB = (b.attr(valueAttribute) || "").toLowerCase()		
+		const valueA = (getValue(a, valueAttribute, valueSelector) || "").toLowerCase();
+		const valueB = (getValue(b, valueAttribute, valueSelector) || "").toLowerCase()		
 		if (valueA < valueB)
 			return -1;
 		if (valueA > valueB)
